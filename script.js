b04a1922a112169c5d5bc0a896ace56bcdec4ca5
aidensmirk <aidenangulu@gmail.com>
2025-02-26 20:39:51 +0300

    software intro capstone

--- script.js
+++ script.js
@@ -2,3 +2,12 @@
 import { Input, initMDB } from "mdb-ui-kit";
 
 initMDB({ Input });
+
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
